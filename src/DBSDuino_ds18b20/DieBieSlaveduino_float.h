817c2721f09cc1179df19a1754b4e5e947ab21a2
fcaminad <fcaminad@laas.fr>
2019-09-27 12:34:47 +0200

    ds18b20 code

---
+++ src/DBSDuino_ds18b20/DieBieSlaveduino_float.h
@@ -0,0 +1,38 @@
+#ifndef CUSTOM_PDO_NAME_H
+#define CUSTOM_PDO_NAME_H
+
+//-------------------------------------------------------------------//
+//                                                                   //
+//     This file has been created by the Easy Configurator tool      //
+//                                                                   //
+//     Easy Configurator project DieBieSlaveduino_float.prj
+//                                                                   //
+//-------------------------------------------------------------------//
+
+
+#define CUST_BYTE_NUM_OUT	1
+#define CUST_BYTE_NUM_IN	4
+#define TOT_BYTE_NUM_ROUND_OUT	4
+#define TOT_BYTE_NUM_ROUND_IN	4
+
+
+typedef union												//---- output buffer ----
+{
+	uint8_t  Byte [TOT_BYTE_NUM_ROUND_OUT];
+	struct
+	{
+		uint8_t     OutputVariable_uint8;
+	}Cust;
+} PROCBUFFER_OUT;
+
+
+typedef union												//---- input buffer ----
+{
+	uint8_t  Byte [TOT_BYTE_NUM_ROUND_IN];
+	struct
+	{
+		float       InputVariable_float;
+	}Cust;
+} PROCBUFFER_IN;
+
+#endif
